c9bce8cfe31a4f86c42f31d5e7d5357ab0314aff
ebinezh <ebinezhmohan.eee2020@citchennai.net>
2023-03-06 14:49:10 +0530

    Ebi Gobly Google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+import { environment } from 'src/environments/environment.development';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from './header/header.component';
+import { ResultComponent } from './result/result.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    HeaderComponent,
+    ResultComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  searchKey: 'AIzaSyCDHzV3Hn92XqAcQlaVljD8jY_laPQe0W0',
+  firebaseConfig: {
+    apiKey: "AIzaSyCDHzV3Hn92XqAcQlaVljD8jY_laPQe0W0",
+  authDomain: "ebigogleclone.firebaseapp.com",
+  projectId: "ebigogleclone",
+  storageBucket: "ebigogleclone.appspot.com",
+  messagingSenderId: "766857922202",
+  appId: "1:766857922202:web:d46340a437e4aba2fa4123",
+  measurementId: "G-VDNY9YZ3XL"
+  }
+};
